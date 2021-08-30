1bf3a26da5c5517601b02e19acadc02ebc3f0e51
L8NATE <84718195+L8NATE@users.noreply.github.com>
2021-08-31 00:15:58 +0100

    Add files via upload

---
+++ logical_operators.py
@@ -0,0 +1,9 @@
+jarrod_hunrgy = True
+jarrod_tired = True
+
+if jarrod_hunrgy and jarrod_tired:
+    print("Jarrod is hangry!")
+
+jarrod_tired = False
+if jarrod_tired or jarrod_hungry:
+    print("Jarrod is grumpy")
